82d066b370d8841a72f173378c04458c8ca1952c
yogaBytes <meganhollingsheadfan@gmail.com>
2026-08-26 08:40:22 -0400

    chatMessage.java

---
+++ ChatMessage.java
@@ -0,0 +1,20 @@
+package com.voiceactorstudio;
+
+public class ChatMessage {
+
+    private String sender;
+    private String message;
+
+    public ChatMessage(String sender, String message) {
+        this.sender = sender;
+        this.message = message;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
