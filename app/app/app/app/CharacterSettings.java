08bb39f820e2480db4715bae7ed6bae34cde998e
yogaBytes <meganhollingsheadfan@gmail.com>
2026-08-26 11:23:06 -0400

    Update CharacterSettings.java

--- app/app/app/app/CharacterSettings.java
+++ app/app/app/app/CharacterSettings.java
@@ -1,1 +1,28 @@
+package com.voiceactorstudio;
 
+public class CharacterSettings {
+
+    private boolean rememberConversation;
+    private boolean useCharacterGreeting;
+
+    public CharacterSettings() {
+        rememberConversation = true;
+        useCharacterGreeting = true;
+    }
+
+    public boolean isRememberConversation() {
+        return rememberConversation;
+    }
+
+    public void setRememberConversation(boolean remember) {
+        rememberConversation = remember;
+    }
+
+    public boolean isUseCharacterGreeting() {
+        return useCharacterGreeting;
+    }
+
+    public void setUseCharacterGreeting(boolean useGreeting) {
+        useCharacterGreeting = useGreeting;
+    }
+}
