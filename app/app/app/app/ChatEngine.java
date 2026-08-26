3bf38e6e6ce67e10043292af0654d245eeac7051
yogaBytes <meganhollingsheadfan@gmail.com>
2026-08-26 11:09:06 -0400

    Update ChatEngine.java

--- app/app/app/app/ChatEngine.java
+++ app/app/app/app/ChatEngine.java
@@ -1,1 +1,9 @@
+package com.voiceactorstudio;
 
+public interface ChatEngine {
+
+    ChatResponse generateResponse(
+            Character character,
+                  String userMessage
+    );
+}
