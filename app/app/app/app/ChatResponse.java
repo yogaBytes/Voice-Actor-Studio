9a21dedbc8c8311576bc1a42182136c163c2e37a
yogaBytes <meganhollingsheadfan@gmail.com>
2026-08-26 11:07:07 -0400

    Update ChatResponse.java

--- app/app/app/app/ChatResponse.java
+++ app/app/app/app/ChatResponse.java
@@ -1,1 +1,14 @@
+package com.voiceactorstudio;
 
+public class ChatResponse {
+
+    private final String text;
+
+    public ChatResponse(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+          return text;
+    }
+}
