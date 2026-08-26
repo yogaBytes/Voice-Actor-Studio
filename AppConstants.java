39554090861de5c200aff3944312e430fedd69fb
yogaBytes <meganhollingsheadfan@gmail.com>
2026-08-26 11:31:30 -0400

    Update AppConstants.java

--- AppConstants.java
+++ AppConstants.java
@@ -1,1 +1,12 @@
+package com.voiceactorstudio;
 
+public final class AppConstants {
+
+    private AppConstants() {
+        // Prevent accidental creation of this class.
+    }
+
+    public static final String APP_NAME = "Voice Actor Studio";
+    public static final String DEFAULT_CHARACTER_NAME = "New Character";
+    public static final String DEFAULT_GREETING = "Hello! Welcome to Voice Actor Studio.";
+}
