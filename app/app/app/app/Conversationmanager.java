2a281a4c7e0296556e638d49f8d81e47ede6fc6d
yogaBytes <meganhollingsheadfan@gmail.com>
2026-08-26 10:29:00 -0400

    Update Conversationmanager.java

--- app/app/app/app/Conversationmanager.java
+++ app/app/app/app/Conversationmanager.java
@@ -1,1 +1,27 @@
+package com.voiceactorstudio;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConversationManager {
+
+    private final List<ChatMessage> messages = new ArrayList<>();
+
+    public void addMessage(ChatMessage message) {
+        if (message != null) {
+            messages.add(message);
+        }
+    }
+
+    public List<ChatMessage> getMessages() {
+        return new ArrayList<>(messages);
+    }
+
+    public void clearConversation() {
+        messages.clear();
+    }
+
+    public int getMessageCount() {
+          return messages.size();
+    }
+}
