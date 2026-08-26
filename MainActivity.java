dfc851a5d3dad3e08b042c0f053a078aee2da307
yogaBytes <meganhollingsheadfan@gmail.com>
2026-08-25 21:56:03 -0400

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,1 +1,35 @@
+package com.voiceactorstudio
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            VoiceActorStudio()
+        }
+    }
+}
+
+@Composable
+fun VoiceActorStudio() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("Voice Actor Studio")
+        Text("Your offline character studio")
+    }
+}          
