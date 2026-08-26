8d2f79d493296ec30da59e2b18864071a63d4d74
yogaBytes <meganhollingsheadfan@gmail.com>
2026-08-26 11:56:40 -0400

    Update AppState.java

--- AppState.java
+++ AppState.java
@@ -1,1 +1,29 @@
+package com.voiceactorstudio;
 
+public class AppState {
+
+    private Character selectedCharacter;
+    private ChatSession currentChatSession;
+
+    public Character getSelectedCharacter() {
+        return selectedCharacter;
+    }
+
+    public void setSelectedCharacter(Character character) {
+        selectedCharacter = character;
+
+        if (character != null) {
+            currentChatSession = new ChatSession(character);
+        } else {
+            currentChatSession = null;
+        }
+    }
+
+    public ChatSession getCurrentChatSession() {
+        return currentChatSession;
+    }
+
+    public boolean hasSelectedCharacter() {
+        return selectedCharacter != null;
+    }
+}
