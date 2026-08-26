0d0a4e4d7b6f5aa89813af293c760ca12da237bd
yogaBytes <meganhollingsheadfan@gmail.com>
2026-08-26 08:33:53 -0400

    Update character.java

--- character.java
+++ character.java
@@ -1,1 +1,26 @@
+package com.voiceactorstudio;
 
+public class Character {
+
+    private String name;
+    private String personality;
+    private String greeting;
+
+    public Character(String name, String personality, String greeting) {
+        this.name = name;
+        this.personality = personality;
+        this.greeting = greeting;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPersonality() {
+        return personality;
+    }
+
+    public String getGreeting() {
+        return greeting;
+            }
+}
