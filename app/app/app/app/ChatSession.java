45f6343573dabab378106607c4633b8810875394
yogaBytes <meganhollingsheadfan@gmail.com>
2026-08-26 11:05:17 -0400

    Update ChatSession.java

--- app/app/app/app/ChatSession.java
+++ app/app/app/app/ChatSession.java
@@ -1,1 +1,32 @@
+package com.voiceactorstudio;
 
+public class ChatSession {
+
+    private final Character character;
+    private final ConversationManager conversationManager;
+
+    public ChatSession(Character character) {
+        this.character = character;
+        this.conversationManager = new ConversationManager();
+    }
+
+    public Character getCharacter() {
+        return character;
+    }
+
+    public ConversationManager getConversationManager() {
+        return conversationManager;
+    }
+
+    public void addUserMessage(String message) {
+        conversationManager.addMessage(
+                new ChatMessage("You", message)
+        );
+    }
+
+    public void addCharacterMessage(String message) {
+        conversationManager.addMessage(
+                    new ChatMessage(character.getName(), message)
+        );
+    }
+}
