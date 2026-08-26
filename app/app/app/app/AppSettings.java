1ea71a72a57a0cfa97e010805fc8176d645745b7
yogaBytes <meganhollingsheadfan@gmail.com>
2026-08-26 10:38:26 -0400

    Update AppSettings.java

--- app/app/app/app/AppSettings.java
+++ app/app/app/app/AppSettings.java
@@ -1,1 +1,18 @@
+package com.voiceactorstudio;
 
+public class AppSettings {
+
+    private boolean kidsModeEnabled;
+
+    public AppSettings() {
+        kidsModeEnabled = false;
+    }
+
+    public boolean isKidsModeEnabled() {
+        return kidsModeEnabled;
+    }
+
+    public void setKidsModeEnabled(boolean enabled) {
+        kidsModeEnabled = enabled;
+    }
+}
