e8b2b8da5f5f6f5837c0772fe8f95fc41caec013
yogaBytes <meganhollingsheadfan@gmail.com>
2026-08-26 11:46:23 -0400

    Update ChatResolve.java

--- ChatResolve.java
+++ ChatResolve.java
@@ -1,1 +1,38 @@
+package com.voiceactorstudio;
 
+public class ChatResult {
+
+    private final boolean successful;
+    private final ChatResponse response;
+    private final String errorMessage;
+
+    private ChatResult(
+            boolean successful,
+            ChatResponse response,
+            String errorMessage) {
+
+        this.successful = successful;
+        this.response = response;
+        this.errorMessage = errorMessage;
+    }
+
+    public static ChatResult success(ChatResponse response) {
+        return new ChatResult(true, response, "");
+    }
+
+    public static ChatResult failure(String errorMessage) {
+        return new ChatResult(false, null, errorMessage);
+    }
+
+    public boolean isSuccessful() {
+        return successful;
+    }
+
+    public ChatResponse getResponse() {
+        return response;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+}
