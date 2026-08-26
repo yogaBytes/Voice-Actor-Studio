809171a1d554b0f8861185a37051a8acee03dbb9
yogaBytes <meganhollingsheadfan@gmail.com>
2026-08-26 11:20:28 -0400

    Update CharacterResponseStyle.java

--- app/app/app/app/CharacterResponseStyle.java
+++ app/app/app/app/CharacterResponseStyle.java
@@ -1,1 +1,20 @@
+package com.voiceactorstudio;
 
+public class CharacterResponseStyle {
+
+    private final String styleName;
+    private final String description;
+
+    public CharacterResponseStyle(String styleName, String description) {
+        this.styleName = styleName;
+        this.description = description;
+    }
+
+    public String getStyleName() {
+        return styleName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
