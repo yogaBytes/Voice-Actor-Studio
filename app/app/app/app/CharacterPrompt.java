3fa7a475d770e3642e3ffc2d4c9786838847bc50
yogaBytes <meganhollingsheadfan@gmail.com>
2026-08-26 11:15:46 -0400

    Update CharacterPrompt.java

--- app/app/app/app/CharacterPrompt.java
+++ app/app/app/app/CharacterPrompt.java
@@ -1,1 +1,30 @@
+package com.voiceactorstudio;
 
+public class CharacterPrompt {
+
+    private final String characterName;
+    private final String personality;
+    private final String background;
+
+    public CharacterPrompt(
+            String characterName,
+            String personality,
+            String background) {
+
+        this.characterName = characterName;
+        this.personality = personality;
+        this.background = background;
+    }
+
+    public String getCharacterName() {
+        return characterName;
+    }
+
+    public String getPersonality() {
+        return personality;
+    }
+
+    public String getBackground() {
+        return background;
+    }
+}
