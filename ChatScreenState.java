7709515f679e15057f08d9a3291376a805dd1e4f
yogaBytes <meganhollingsheadfan@gmail.com>
2026-08-26 11:59:06 -0400

    Update ChatScreenState.java

--- ChatScreenState.java
+++ ChatScreenState.java
@@ -1,1 +1,38 @@
+package com.voiceactorstudio;
 
+public class ChatScreenState {
+
+    private String characterName;
+    private String statusMessage;
+    private boolean sendingMessage;
+
+    public ChatScreenState() {
+        characterName = "";
+        statusMessage = "";
+        sendingMessage = false;
+    }
+
+    public String getCharacterName() {
+        return characterName;
+    }
+
+    public void setCharacterName(String name) {
+        characterName = name;
+    }
+
+    public String getStatusMessage() {
+        return statusMessage;
+    }
+
+    public void setStatusMessage(String message) {
+        statusMessage = message;
+    }
+
+    public boolean isSendingMessage() {
+        return sendingMessage;
+    }
+
+    public void setSendingMessage(boolean sending) {
+        sendingMessage = sending;
+    }
+}
