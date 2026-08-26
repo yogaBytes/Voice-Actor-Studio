f93671418c2cb7e1d3de826caa4910375293edcd
yogaBytes <meganhollingsheadfan@gmail.com>
2026-08-26 11:28:32 -0400

    Update CharacterProfile.java

--- app/app/app/app/CharacterProfile.java
+++ app/app/app/app/CharacterProfile.java
@@ -1,1 +1,37 @@
+package com.voiceactorstudio;
 
+public class CharacterProfile {
+
+    private final String name;
+    private final String personality;
+    private final String background;
+    private final String greeting;
+
+    public CharacterProfile(
+            String name,
+            String personality,
+            String background,
+            String greeting) {
+
+        this.name = name;
+        this.personality = personality;
+        this.background = background;
+        this.greeting = greeting;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPersonality() {
+        return personality;
+    }
+
+    public String getBackground() {
+        return background;
+    }
+
+    public String getGreeting() {
+        return greeting;
+    }
+}
